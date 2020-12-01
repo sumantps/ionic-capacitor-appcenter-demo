8cade7d98b39a467663ad17c22656e607ef71bf8
sumantps <47108862+sumantps@users.noreply.github.com>
2020-12-01 13:12:17 -0500

    Update appcenter-post-clone.sh

--- ios/App/appcenter-post-clone.sh
+++ ios/App/appcenter-post-clone.sh
@@ -6,13 +6,13 @@ set -e
 set -x
 
 # Required nodeJS version
-NODE_VERSION=10.17.0
+NODE_VERSION=14.15.1
 
 # workaround to override the v8 alias
 npm config delete prefix
 . ~/.bashrc
 nvm install "$NODE_VERSION"
-nvm alias node10 "$NODE_VERSION"
+nvm alias node14 "$NODE_VERSION"
 
 # go to root of project
 cd ../..
